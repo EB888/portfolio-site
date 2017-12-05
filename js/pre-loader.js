5642fec3474be6f8d9f03ff229bf6df2116d43a9
LaserHawk <laserhawk@LaserHawks-MacBook-Pro.local>
2017-12-05 15:31:31 -0800

    add pre-loader with jQuery, css class with gif background image

---
+++ js/pre-loader.js
@@ -0,0 +1,10 @@
+$(function() {
+  // Wait for window load
+  $(window).on('load', function() {
+
+    // Animate loader off screen
+    $(".se-pre-con").delay(3000).fadeOut(200);
+    console.log("TEST");
+  });
+
+});
